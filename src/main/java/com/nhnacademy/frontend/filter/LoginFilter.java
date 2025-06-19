564c4f78d03017f0d7dec554a7841ab2c960b1c9
NaIlHwan <nlhjobs01@naver.com>
2025-06-19 09:40:39 +0900

    feat: LoginFilter 로그메시지 추가

--- src/main/java/com/nhnacademy/frontend/filter/LoginFilter.java
+++ src/main/java/com/nhnacademy/frontend/filter/LoginFilter.java
@@ -5,6 +5,7 @@ import com.nhnacademy.frontend.service.AuthService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,7 +17,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import java.io.IOException;
 import java.util.Collections;
 
-
+@Slf4j
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
     private final AuthService authService;
 
@@ -31,16 +32,19 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        log.debug("로그인 시도: username={}, password={}", username, password);
 
         // 1. AuthService로 인증 요청 및 토큰 수령
         LoginResponseDto loginResponse = authService.login(username, password);
 
         if(loginResponse != null && loginResponse.getAccessToken() != null) {
             // 2. 인증 객체 생성 (details에 토큰 정보 저장)
+            log.info("로그인 성공: username={}", username);
             UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
             authToken.setDetails(loginResponse);
             return authToken;
         }
+        log.warn("로그인 실패: username={}", username);
         throw new BadCredentialsException("아이디 또는 비밀번호가 올바르지 않습니다.");
     }
 
