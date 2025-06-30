9f1dfd7bdb516fe6abe902138f64c92587e3be6c
JYCuzz <hanafos944@naver.com>
2025-06-30 15:08:30 +0900

    feat: redis 를 활용하여 세션에 저장된 정보가 유실되지 않게 구현

--- src/test/java/com/nhnacademy/frontend/auth/integration/AuthIntegrationTest.java
+++ src/test/java/com/nhnacademy/frontend/auth/integration/AuthIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AuthIntegrationTest {
 
     @Autowired
