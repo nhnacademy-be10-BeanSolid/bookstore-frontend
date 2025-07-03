86d211af6d5a7ac393e550efe1c31e3d7d2084dc
JYCuzz <hanafos944@naver.com>
2025-07-03 11:15:47 +0900

    feat : 에러 페이지 테스트 코드 구현 완료

--- src/main/java/com/nhnacademy/frontend/user/controller/UserTestController.java
+++ src/main/java/com/nhnacademy/frontend/user/controller/UserTestController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Objects;
+
 @Controller
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -22,7 +24,7 @@ public class UserTestController {
 
         UserResponseDto responseDto = userAdapter.getUser(userId).getBody();
 
-        model.addAttribute("userId", responseDto.getUserName());
+        model.addAttribute("userId", Objects.requireNonNull(responseDto).getUserName());
         return "user-test";
     }
 }
