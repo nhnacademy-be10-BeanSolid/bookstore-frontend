66954f26139bedbfbf918efd0c24394c76fd5752
JYCuzz <hanafos944@naver.com>
2025-07-03 15:36:00 +0900

    feat : 에러 페이지 풀 리퀘스트 comment 반영

--- src/test/java/com/nhnacademy/frontend/auth/controller/SignupControllerTest.java
+++ src/test/java/com/nhnacademy/frontend/auth/controller/SignupControllerTest.java
@@ -89,7 +89,7 @@ class SignupControllerTest {
     @DisplayName("회원가입 폼 GET - userIdCheckRequestDto 없는 경우")
     @Test
     void signupForm_addsUserIdCheckRequestDto() throws Exception {
-        mockMvc.perform(get("/auth/normal-signup"))
+        mockMvc.perform(get("/auth/signup/normal-signup"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("userIdCheckRequestDto"))
                 .andExpect(view().name("auth/normal-signup"));
@@ -98,7 +98,7 @@ class SignupControllerTest {
     @DisplayName("회원가입 선택 폼 GET")
     @Test
     void showSelectSignupForm() throws Exception {
-        mockMvc.perform(get("/auth/select-signup"))
+        mockMvc.perform(get("/auth/signup/select-signup"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("auth/select-signup"));
     }
@@ -108,12 +108,12 @@ class SignupControllerTest {
     void checkUserId_duplicate() throws Exception {
         given(userService.isExistUser("testuser")).willReturn(true);
 
-        mockMvc.perform(post("/auth/check-user-id")
+        mockMvc.perform(post("/auth/signup/check-user-id")
                         .param("userId", "testuser"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(flash().attribute("userId", "testuser"))
                 .andExpect(flash().attribute("duplicateMessage", "이미 사용 중인 아이디입니다."))
-                .andExpect(redirectedUrl("/auth/normal-signup"));
+                .andExpect(redirectedUrl("/auth/signup/normal-signup"));
     }
 
     @DisplayName("아이디 중복확인 - 사용 가능한 아이디")
@@ -121,18 +121,18 @@ class SignupControllerTest {
     void checkUserId_available() throws Exception {
         given(userService.isExistUser("newuser")).willReturn(false);
 
-        mockMvc.perform(post("/auth/check-user-id")
+        mockMvc.perform(post("/auth/signup/check-user-id")
                         .param("userId", "newuser"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(flash().attribute("userId", "newuser"))
                 .andExpect(flash().attribute("duplicateMessage", "사용 가능한 아이디입니다."))
-                .andExpect(redirectedUrl("/auth/normal-signup"));
+                .andExpect(redirectedUrl("/auth/signup/normal-signup"));
     }
 
     @DisplayName("회원가입 처리 - 중복확인 안함")
     @Test
     void registerUser_duplicateCheckNotPassed() throws Exception {
-        mockMvc.perform(post("/auth/normal-signup/register")
+        mockMvc.perform(post("/auth/signup/normal-signup/register")
                         .param("userId", "user1")
                         .param("userPassword", "pw1234")
                         .param("userName", "홍길동")
@@ -140,7 +140,7 @@ class SignupControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(flash().attribute("duplicateMessage", "아이디 중복 문제 먼저 해결해주세요."))
                 .andExpect(flash().attribute("userId", "user1"))
-                .andExpect(redirectedUrl("/auth/normal-signup"));
+                .andExpect(redirectedUrl("/auth/signup/normal-signup"));
     }
 
     @DisplayName("회원가입 처리 - 정상 플로우")
@@ -148,7 +148,7 @@ class SignupControllerTest {
     void registerUser_success() throws Exception {
         doNothing().when(userService).register(ArgumentMatchers.any(UserCreateRequestDto.class));
 
-        mockMvc.perform(post("/auth/normal-signup/register")
+        mockMvc.perform(post("/auth/signup/normal-signup/register")
                         .param("userId", "user2")
                         .param("userPassword", "pw1234")
                         .param("userName", "홍길동")
