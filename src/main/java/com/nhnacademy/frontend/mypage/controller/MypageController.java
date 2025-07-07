b44c0593eb2f0ebfd7b62177771e1cb0d31397ba
NaIlHwan <nlhjobs01@naver.com>
2025-07-07 16:06:28 +0900

    feat: MypageController import문 최적화

--- src/main/java/com/nhnacademy/frontend/mypage/controller/MypageController.java
+++ src/main/java/com/nhnacademy/frontend/mypage/controller/MypageController.java
@@ -1,22 +1,17 @@
 package com.nhnacademy.frontend.mypage.controller;
 
 
+import com.nhnacademy.frontend.auth.util.JwtCookieUtil;
 import com.nhnacademy.frontend.common.adapter.domain.response.ResponseUser;
 import com.nhnacademy.frontend.mypage.service.MypageService;
 import com.nhnacademy.frontend.user.domain.request.UserUpdateRequestDto;
 import com.nhnacademy.frontend.user.service.UserService;
-import com.nhnacademy.frontend.auth.util.JwtCookieUtil;
-import com.nhnacademy.frontend.mypage.service.MypageService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/mypage")
