9f690236c8772a67e2a5ca22440f25b93bf35aed
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-08 11:27:23 +0900

    feat-PaymentAdapter - name : gateway-service로 수정

--- src/main/java/com/nhnacademy/frontend/payment/adapter/PaymentAdapter.java
+++ src/main/java/com/nhnacademy/frontend/payment/adapter/PaymentAdapter.java
@@ -6,24 +6,23 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(
-        name      = "order-api",          // 아무 식별자
-        contextId = "paymentsAdapter",
-        url       = "${order-api.base-url}"   // 반드시 ${…}
+        name      = "gateway-service",
+        contextId = "paymentsAdapter"
 )
 public interface PaymentAdapter {
 
     /** 결제 준비(redirect URL 받기) */
-    @PostMapping("/payments/toss/{orderId}")
+    @PostMapping("/order-api/payments/toss/{orderId}")
     PaymentResponseDto ready(@PathVariable("orderId") String orderId,
                              @RequestBody PaymentRequestDto dto);
 
     /** 결제 성공 콜백 → 최종 confirm */
-    @PostMapping("/payments/toss/success")
+    @PostMapping("/order-api/payments/toss/success")
     void confirmSuccess(@RequestParam("paymentKey") String paymentKey,
                         @RequestParam("orderId")    String orderId);
 
     /** 결제 실패 콜백 → 실패 처리 */
-    @PostMapping("/payments/toss/fail")
+    @PostMapping("/order-api/payments/toss/fail")
     void confirmFail(@RequestParam("paymentKey") String paymentKey,
                      @RequestParam("orderId")    String orderId);
 }
