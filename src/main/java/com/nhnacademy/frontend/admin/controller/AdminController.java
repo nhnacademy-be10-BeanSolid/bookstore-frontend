5b1e83804b7e61fa9eba9d47f6bb2896fffb8f26
JYCuzz <hanafos944@naver.com>
2025-07-10 14:56:27 +0900

    feat : 포인트 규정 CRUD, 포인트 내역 조회 기능, 몇몇 페이지 부트스트랩 5 적용

--- src/main/java/com/nhnacademy/frontend/admin/controller/AdminController.java
+++ src/main/java/com/nhnacademy/frontend/admin/controller/AdminController.java
@@ -1,20 +1,7 @@
 package com.nhnacademy.frontend.admin.controller;
 
-import com.nhnacademy.frontend.admin.domain.request.PointTypeCreateRequestDto;
-import com.nhnacademy.frontend.admin.domain.request.PointTypeUpdateRequestDto;
-import com.nhnacademy.frontend.admin.domain.response.ResponsePointType;
-import com.nhnacademy.frontend.admin.service.AdminService;
-import com.nhnacademy.frontend.common.exception.ValidationFailedException;
-import feign.FeignException;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
