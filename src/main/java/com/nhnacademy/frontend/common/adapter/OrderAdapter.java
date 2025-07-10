433edbc11c7708c6855512bd6576f37ea5d1dad6
JYCuzz <hanafos944@naver.com>
2025-07-10 10:27:44 +0900

    feat : frontend 포인트 및 유형별 적립 포인트 구현중

--- src/main/java/com/nhnacademy/frontend/common/adapter/OrderAdapter.java
+++ src/main/java/com/nhnacademy/frontend/common/adapter/OrderAdapter.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "gateway-service", contextId = "orderAdapter")
+@FeignClient(name = "gateway-service", contextId = "orderAdapter1")
 public interface OrderAdapter {
     
     @PostMapping("/order-api/orders")
