248eb92edae64e3f00b33b7441d2dbb29a3678e5
mni-js <2ujs4311@gmail.com>
2025-07-11 17:13:47 +0900

    style: bootstrap 적용 및 bean validation 패턴 바꿈. - 회원 db에서 휴대폰 번호를 빼기(-)를 포함해서 저장하고 있어서 bean validation 패턴을 바꿈. - 일관된 페이지 디자인을 가지기 위해 적절하게 부트스트랩 적용.

--- src/main/java/com/nhnacademy/frontend/order/dto/request/OrderRequest.java
+++ src/main/java/com/nhnacademy/frontend/order/dto/request/OrderRequest.java
@@ -15,8 +15,8 @@ public class OrderRequest {
     private String receiverName;
 
     @NotBlank(message = "받는 사람 전화번호를 입력해주세요")
-    @Pattern(regexp = "^01[0-9][0-9]{8}$",
-            message = "올바른 휴대폰 번호 형식이 아닙니다 (올바른 형식: 01012345678)")
+    @Pattern(regexp = "^01\\d-\\d{4}-\\d{4}$",
+            message = "올바른 휴대폰 번호 형식이 아닙니다 (올바른 형식: 010-1234-5678)")
     private String receiverPhoneNumber;
 
     @NotBlank(message = "배송지를 선택해주세요")
