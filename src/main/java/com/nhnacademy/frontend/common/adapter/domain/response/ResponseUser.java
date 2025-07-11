0ec68ee635f0fb9259ecda107a3ad1a4c72d70d1
yooncy <yeori21@naver.com>
2025-07-11 17:39:16 +0900

    feat(test, oauth2) : 테스트 코드 작성 및 작성중 발견된 오류 사항 수정(OAuth2유저의 개인정보 수정 시 수정api가 호출 되지 않던 상황 수정) Fixes#67

--- src/main/java/com/nhnacademy/frontend/common/adapter/domain/response/ResponseUser.java
+++ src/main/java/com/nhnacademy/frontend/common/adapter/domain/response/ResponseUser.java
@@ -25,4 +25,21 @@ public class ResponseUser {
     public String getGradeName() {
         return this.userGradeName;
     }
+    public ResponseUser(Long userNo, String userId, String userPassword, String userName,
+                        String userPhoneNumber, String userEmail, LocalDate userBirth,
+                        int userPoint, boolean isAuth, String userStatus,
+                        LocalDateTime lastLoginAt, String userGradeName) {
+        this.userNo = userNo;
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.userName = userName;
+        this.userPhoneNumber = userPhoneNumber;
+        this.userEmail = userEmail;
+        this.userBirth = userBirth;
+        this.userPoint = userPoint;
+        this.isAuth = isAuth;
+        this.userStatus = userStatus;
+        this.lastLoginAt = lastLoginAt;
+        this.userGradeName = userGradeName;
+    }
 }
