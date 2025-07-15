a0d76f11f4f603805370960f0e316e5c9173a2e0
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-15 15:53:02 +0900

    feat: JSON 날짜 문자열 파싱 지원을 위한 LocalDateTimeDeserializer추가

---
+++ src/main/java/com/nhnacademy/frontend/common/util/LocalDateTimeSerializer.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.frontend.common.util;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeString(value.format(FORMATTER));
+    }
+}
