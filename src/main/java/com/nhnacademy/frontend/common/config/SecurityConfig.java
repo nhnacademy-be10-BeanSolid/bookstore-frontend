a56e5501fdb7f0ea321b0f1e9d2b437d95e8c8fe
chosun-nhn16 <tmdals3622@naver.com>
2025-07-16 15:18:11 +0900

    refactor: 필터 위치 변경

--- src/main/java/com/nhnacademy/frontend/common/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/frontend/common/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -23,7 +22,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http,
                                                    JwtAuthenticationFilter jwtAuthenticationFilter,
-                                                   HiddenHttpMethodFilter hiddenHttpMethodFilter,
                                                    AuthService authService,
                                                    LoginSuccessHandler successHandler,
                                                    CustomCookieClearingLogoutHandler customCookieClearingLogoutHandler) throws Exception {
@@ -70,7 +68,6 @@ public class SecurityConfig {
                         .addLogoutHandler(customCookieClearingLogoutHandler)
                         .logoutSuccessUrl("/")
                 )
-                .addFilterBefore(hiddenHttpMethodFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session ->
