b7a499749c83db216a47782515e84adab0945b70
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-17 10:06:46 +0900

    feat: 문자열을 받아오면 안되기 때문에 CustomPrincipal 객체를 받아옴

--- src/main/java/com/nhnacademy/frontend/coupon/controller/CouponController.java
+++ src/main/java/com/nhnacademy/frontend/coupon/controller/CouponController.java
@@ -29,7 +29,7 @@ public class CouponController {
             // 사용자가 인증되지 않은 경우 로그인 페이지로 리다이렉트 또는 에러 처리
             return "redirect:/auth/login";
         }
-        com.nhnacademy.frontend.auth.principal.CustomPrincipal customPrincipal = (com.nhnacademy.frontend.auth.principal.CustomPrincipal) authentication.getPrincipal();
+        CustomPrincipal customPrincipal = (CustomPrincipal) authentication.getPrincipal();
         String userNo = customPrincipal.getUsername();
         log.info("Frontend CouponController: Fetching coupons for userNo (from Authentication): {}", userNo);
 
