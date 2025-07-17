3364e23d3fab047c455cbc00cd792ab1ef6f137a
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-17 10:25:55 +0900

    feat: gateway-service 거쳐서 prontend

--- src/main/java/com/nhnacademy/frontend/common/adapter/CouponAdapter.java
+++ src/main/java/com/nhnacademy/frontend/common/adapter/CouponAdapter.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "coupon-api")
+@FeignClient(name = "gateway-service", contextId = "couponAdapter")
 public interface CouponAdapter {
 
     @GetMapping("/coupons/users/{userNo}/active")
