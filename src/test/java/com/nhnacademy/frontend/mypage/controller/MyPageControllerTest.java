a21f65fe1a9d985ac9d2af5be39324d2a8acd7c9
mni-js <2ujs4311@gmail.com>
2025-07-18 10:42:50 +0900

    fix: 테스트 코드 경로 문제 해결

--- src/test/java/com/nhnacademy/frontend/mypage/controller/MyPageControllerTest.java
+++ src/test/java/com/nhnacademy/frontend/mypage/controller/MyPageControllerTest.java
@@ -497,7 +497,7 @@ public class MyPageControllerTest {
         // When & Then
         mockMvc.perform(get("/mypage/orders/{orderNumber}", orderNumber))
                 .andExpect(status().isOk())
-                .andExpect(view().name("mypage/detail"))
+                .andExpect(view().name("mypage/order-detail"))
                 .andExpect(model().attribute("order", orderDetail));
 
         Mockito.verify(mypageService).getOrderDetail(orderNumber);
