5c0f8e90b5ea8440319fb6777d9e7ee7b889971d
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-21 13:31:22 +0900

    feat: 관리자-> 책의 정보 및 유저정보 받아오기 위한 작성

--- src/main/java/com/nhnacademy/frontend/common/adapter/CouponAdapter.java
+++ src/main/java/com/nhnacademy/frontend/common/adapter/CouponAdapter.java
@@ -1,10 +1,13 @@
 package com.nhnacademy.frontend.common.adapter;
 
+import com.nhnacademy.frontend.coupon.dto.CouponPolicyResponse;
 import com.nhnacademy.frontend.coupon.dto.UserCouponResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,4 +16,19 @@ public interface CouponAdapter {
 
     @GetMapping("/coupon-api/coupons/users/{userNo}/active")
     ResponseEntity<List<UserCouponResponse>> getActiveUserCoupons(@PathVariable("userNo") Long userNo);
+
+    @GetMapping("/coupon-api/coupons/policy")
+    List<CouponPolicyResponse> getAllCouponPolicies();
+
+    @PostMapping("/coupon-api/admin/issue-all/{couponPolicyId}")
+    void startIssuingCouponsToAllUsers(@PathVariable("couponPolicyId") Long couponPolicyId);
+
+    @PostMapping("/coupon-api/admin/issue-book")
+    void issueCouponToBook(@RequestParam("couponPolicyId") Long couponPolicyId, @RequestParam("bookId") Long bookId);
+
+    @PostMapping("/coupon-api/admin/issue-to-user")
+    void issueCouponToUser(@RequestParam("userNo") Long userNo, @RequestParam("couponPolicyId") Long couponPolicyId);
+
+    @PostMapping("/coupon-api/coupons/issue/book")
+    ResponseEntity<com.nhnacademy.frontend.coupon.dto.UserCouponResponse> issueBookCoupon(com.nhnacademy.dto.IssueBookCouponRequest request);
 }
