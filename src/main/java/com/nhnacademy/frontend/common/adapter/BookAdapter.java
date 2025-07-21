4e08b5dccb33dabe1987f6ffed3ee4047e37190a
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-21 13:30:29 +0900

    feat: 도서에대한 정보 받아오기위한 url

--- src/main/java/com/nhnacademy/frontend/common/adapter/BookAdapter.java
+++ src/main/java/com/nhnacademy/frontend/common/adapter/BookAdapter.java
@@ -4,6 +4,7 @@ import com.nhnacademy.frontend.common.adapter.dto.book.response.SimpleBookRespon
 import com.nhnacademy.frontend.common.adapter.dto.book.request.*;
 import com.nhnacademy.frontend.common.adapter.dto.book.response.*;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
 
@@ -63,11 +64,11 @@ public interface BookAdapter {
 
     // 도서 상세정보
     @GetMapping("/book-api/books/{bookId}")
-    BookDetailResponseDto getBookDetail(@PathVariable Long bookId);
+    BookDetailResponseDto getBookDetail( @PathVariable Long bookId);
 
     // 도서 생성
     @PostMapping("/book-api/books")
-    BookResponseDto createBook(@RequestBody BookCreateRequestDto request);
+    BookResponseDto createBook( @RequestBody BookCreateRequestDto request);
 
     // 외부 도서 검색
     @GetMapping("/book-api/books-search")
@@ -77,47 +78,47 @@ public interface BookAdapter {
 
     // 도서 업데이트
     @PutMapping("/book-api/books/{bookId}")
-    BookDetailResponseDto updateBook(@PathVariable Long bookId, @RequestBody BookUpdateRequestDto request);
+    BookDetailResponseDto updateBook( @PathVariable Long bookId, @RequestBody BookUpdateRequestDto request);
 
     // 도서 삭제
     @DeleteMapping("/book-api/books/{bookId}")
-    void deleteBook(@PathVariable Long bookId);
+    void deleteBook( @PathVariable Long bookId);
 
     // 해당 도서의 태그 조회
     @GetMapping("/book-api/books/{bookId}/tags")
-    BookTagMapResponseDto getBookTagMap(@PathVariable Long bookId);
+    BookTagMapResponseDto getBookTagMap( @PathVariable Long bookId);
 
     // 도서에 태그 추가
     @PostMapping("/book-api/books/{bookId}/tags")
-    void createBookTagMap(@PathVariable Long bookId, @RequestBody BookTagMapCreateRequestDto request);
+    void createBookTagMap( @PathVariable Long bookId, @RequestBody BookTagMapCreateRequestDto request);
 
     // 도서에서 태그 삭제
     @DeleteMapping("/book-api/books/{bookId}/tags/{tagId}")
-    void deleteBookTagMap(@PathVariable Long bookId, @PathVariable Long tagId);
+    void deleteBookTagMap( @PathVariable Long bookId, @PathVariable Long tagId);
 
     // 해당 도서의 카테고리 조회
     @GetMapping("/book-api/books/{bookId}/categories")
-    BookCategoryMapResponseDto getBookCategoryMap(@PathVariable Long bookId);
+    BookCategoryMapResponseDto getBookCategoryMap( @PathVariable Long bookId);
 
     // 도서에 카테고리 추가
     @PostMapping("/book-api/books/{bookId}/categories")
-    void createBookCategoryMap(@PathVariable Long bookId, @RequestBody BookCategoryMapCreateRequestDto request);
+    void createBookCategoryMap( @PathVariable Long bookId, @RequestBody BookCategoryMapCreateRequestDto request);
 
     // 도서에서 카테고리 삭제
     @DeleteMapping("/book-api/books/{bookId}/categories/{categoryId}")
-    void deleteBookCategoryMap(@PathVariable Long bookId, @PathVariable Long categoryId);
+    void deleteBookCategoryMap( @PathVariable Long bookId, @PathVariable Long categoryId);
 
     // 도서 좋아요 생성
     @PostMapping("/book-api/books/{bookId}/bookLikes")
-    BookLikeResponse createBookLike(@PathVariable Long bookId, @RequestHeader String userId);
+    BookLikeResponse createBookLike( @PathVariable Long bookId, @RequestHeader String userId);
 
     // 좋아요 조회
     @GetMapping("/book-api/books/{bookId}/bookLikes")
-    Page<BookLikeResponse> getBookLikes(@PathVariable Long bookId, @RequestParam int page, @RequestParam int size);
+    Page<BookLikeResponse> getBookLikes( @PathVariable Long bookId, @RequestParam int page, @RequestParam int size);
 
     // 좋아요 삭제
     @DeleteMapping("/book-api/books/{bookId}/bookLikes")
-    void deleteBookLike(@PathVariable Long bookId, @RequestHeader String userId);
+    void deleteBookLike( @PathVariable Long bookId, @RequestHeader String userId);
 
     // 엘라스틱 서치
     @GetMapping("/book-api/search")
@@ -132,4 +133,7 @@ public interface BookAdapter {
 
     @GetMapping("/book-api/categories/tree")
     List<BookCategoryNodeResponseDto> getCategoryTree();
+
+    @GetMapping("/book-api/books")
+    Page<BookResponse> getAllBooks(Pageable pageable);
 }
