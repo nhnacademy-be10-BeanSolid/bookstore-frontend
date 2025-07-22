83a84bf1b73734e9d94157de24d5f81e7f696e07
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-22 09:51:03 +0900

    feat: 관리자용 도서 쿠폰 적용

---
+++ src/main/java/com/nhnacademy/frontend/admin/dto/response/BookResponse.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.frontend.admin.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookResponse {
+    private Long bookId;
+    private String title;
+    private String author;
+}
