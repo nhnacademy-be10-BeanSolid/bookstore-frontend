0cb2f290f19a13620064bba4c06986ed5f515490
sminlee123 <154220615+sminlee123@users.noreply.github.com>
2025-07-25 13:30:44 +0900

    Update SecurityConfig.java

--- src/main/java/com/nhnacademy/frontend/common/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/frontend/common/config/SecurityConfig.java
@@ -67,7 +67,6 @@ public class SecurityConfig {
                         .requestMatchers("/books/**").permitAll()
                         .requestMatchers("/images/**").permitAll()
                         .requestMatchers("/reviews/**").permitAll()
-                        .requestMatchers("/reviews/**").permitAll()
                         .requestMatchers("/search/**").permitAll()
                         .requestMatchers("/categories/**").permitAll()
                         .anyRequest().authenticated()
